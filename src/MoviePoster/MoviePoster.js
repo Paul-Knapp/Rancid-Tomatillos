6504d4def26ef213370e0f88a91f062a4fe02ab0
Paul Knapp <ppankluap@gmail.com>
2024-12-16 19:01:37 -0600

    Feat: Upvote and Downvote button.

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -1,6 +1,8 @@
 import './MoviePoster.css';
 import { useState, useEffect } from 'react';
 import moviePosters from '../data/movie_posters';
+import upvote from '../icons/upvote.png'
+import downvote from '../icons/downvote.png'
 
 function MoviePoster({ poster }) {
   return (
@@ -8,6 +10,12 @@ function MoviePoster({ poster }) {
       <div className="movie-poster-container">
         <img src={poster.poster_path} alt={`${poster.title}`} className="movie-poster" />
         <div className="vote-count">{poster.vote_count}</div>
+          <btn className="upvote-button">
+            <img src={upvote} alt="U" img/>
+          </btn>
+          <btn className="downvote-button">
+            <img src={downvote} alt="D" img/>
+          </btn>
       </div>
     </section>
   );
